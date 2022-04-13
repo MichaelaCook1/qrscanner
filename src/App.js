83b02ad5400600a4be00ff061f27e05dfa15f38a
MichaelaCook1 <MCook1@qa.com>
2022-04-13 16:08:08 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import React, { Component } from 'react';
-import QrReader from 'react-qr-scanner';
+import React from 'react';
+import "./styles.scss";
+import QRScan from "./qr-reader/qr-reader.component";
 
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      delay: 100,
-      result: 'No result',
-    }
-
-    this.handleScan = this.handleScan.bind(this)
-  }
-  handleScan(data){
-    this.setState({
-      result: data,
-    })
-  }
-  handleError(err){
-    console.error(err)
-  }
-  render(){
-    const previewStyle = {
-      height: 240,
-      width: 320,
-    }
-
-    return(
-      <div>
-        <QrReader
-          delay={this.state.delay}
-          style={previewStyle}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          />
-        <p>{this.state.result}</p>
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello QR Code Reader</h1>
+      <QRScan />
+    </div>
+  );
 }
-export default App;
